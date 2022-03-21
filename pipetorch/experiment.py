c9944c6012d1d2d7078f2397ca29da51f6687777
omrijsharon <omri_s@yahoo.com>
2022-03-21 22:50:47 +0200

    added cross validation

--- pipetorch/experiment.py
+++ pipetorch/experiment.py
@@ -8,10 +8,11 @@ from datetime import datetime, timedelta
 
 
 class Experiment(object):
-    def __init__(self, model, data_loader, loss_function, optimizer, experiments_path, scheduler=None, data_preprocess_function=None):
+    def __init__(self, model, train_data_loader, eval_data_loader, loss_function, optimizer, experiments_path, scheduler=None, data_preprocess_function=None):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
-        self.data_loader = data_loader
+        self.train_data_loader = train_data_loader
+        self.eval_data_loader = eval_data_loader
         self.loss_function = loss_function
         self.optimizer = optimizer
         self.scheduler = scheduler
@@ -19,16 +20,20 @@ class Experiment(object):
         self.experiment_path = os.path.join(experiments_path, datetime.now().strftime("%Y_%m_%d-%H_%M_%S"))
         if not os.path.exists(self.experiment_path):
             os.mkdir(self.experiment_path)
-        self.writer = SummaryWriter(self.experiment_path)
+            os.mkdir(os.path.join(self.experiment_path, "train"))
+            os.mkdir(os.path.join(self.experiment_path, "eval"))
+        self.train_writer = SummaryWriter(os.path.join(self.experiment_path, "train"))
+        self.eval_writer = SummaryWriter(os.path.join(self.experiment_path, "eval"))
         self.data_preprocess_function = data_preprocess_function
         self.t0 = datetime.now()
 
     def run_single_epoch(self):
         self.epoch += 1
+        data_loader = self.train_data_loader
         self.model.train()
         self.optimizer.zero_grad(set_to_none=True)
-        with tqdm(total=len(self.data_loader)) as pbar:
-            for batch_idx, (data, target) in enumerate(self.data_loader):
+        with tqdm(total=len(data_loader)) as pbar:
+            for batch_idx, (data, target) in enumerate(data_loader):
                 data = data.to(self.device)
                 if self.data_preprocess_function is not None:
                     data = self.data_preprocess_function(data)
@@ -39,15 +44,15 @@ class Experiment(object):
                 accuracy = callback_functions.accuracy(output, target).item()
                 recall, precision, f1_score = callback_functions.recall_precision_f1(output, target)
                 now = datetime.now()
-                pbar.set_description("{} @ Elapsed time: {} @ Epoch: {}, ".format(now.strftime("%d/%m/%Y, %H:%M:%S"), str(now-self.t0)[:-7], self.epoch))
+                pbar.set_description("{} @ Elapsed time: {} @ Epoch: {}, Train".format(now.strftime("%d/%m/%Y, %H:%M:%S"), str(now-self.t0)[:-7], self.epoch))
                 pbar.update(1)
-                self.writer.add_scalar('Metric/Training loss', loss.item(), self.epoch * len(self.data_loader) + batch_idx)
-                self.writer.add_scalar('Metric/Model Entropy', Categorical(logits=output).entropy().mean().item(), self.epoch * len(self.data_loader) + batch_idx)
-                self.writer.add_scalar('Metric/Accuracy', accuracy, self.epoch * len(self.data_loader) + batch_idx)
-                self.writer.add_scalar('Metric/Recall', recall, self.epoch * len(self.data_loader) + batch_idx)
-                self.writer.add_scalar('Metric/Precision', precision, self.epoch * len(self.data_loader) + batch_idx)
-                self.writer.add_scalar('Metric/F1 Score', f1_score, self.epoch * len(self.data_loader) + batch_idx)
-                self.writer.add_scalar('Metric/Gradients mean length', callback_functions.grad_abs_mean(self.model), self.epoch * len(self.data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/Loss', loss.item(), self.epoch * len(data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/Model Entropy', Categorical(logits=output).entropy().mean().item(), self.epoch * len(data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/Accuracy', accuracy, self.epoch * len(data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/Recall', recall, self.epoch * len(data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/Precision', precision, self.epoch * len(data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/F1 Score', f1_score, self.epoch * len(data_loader) + batch_idx)
+                self.train_writer.add_scalar('Metric/Gradients mean length', callback_functions.grad_abs_mean(self.model), self.epoch * len(data_loader) + batch_idx)
 
                 self.optimizer.step()
                 self.optimizer.zero_grad(set_to_none=True)
@@ -58,15 +63,33 @@ class Experiment(object):
         os.popen(r'tensorboard --logdir=' + self.experiment_path)
         while True:
             self.run_single_epoch()
+            self.eval()
             if self.epoch % delta_epochs_to_save_checkpoint == 0:
                 self.save()
 
-    def eval(self, eval_data_loader):
-        self.model.eval()
+    def eval(self):
+        # self.model.eval()
+        data_loader = self.train_data_loader
         with torch.no_grad():
-            for batch_idx, (data, target) in tqdm(enumerate(eval_data_loader)):
-                output = self.model(data)
-                self.writer.add_scalar('training loss', output.item(), self.epoch * len(self.data_loader) + batch_idx)
+            with tqdm(total=len(self.eval_data_loader)) as pbar:
+                for batch_idx, (data, target) in enumerate(self.eval_data_loader):
+                    data = data.to(self.device)
+                    if self.data_preprocess_function is not None:
+                        data = self.data_preprocess_function(data)
+                    target = target.to(self.device)
+                    output = self.model(data)
+                    loss = self.loss_function(output, target)
+                    accuracy = callback_functions.accuracy(output, target).item()
+                    recall, precision, f1_score = callback_functions.recall_precision_f1(output, target)
+                    now = datetime.now()
+                    pbar.set_description("{} @ Elapsed time: {} @ Epoch: {}, Eval ".format(now.strftime("%d/%m/%Y, %H:%M:%S"), str(now - self.t0)[:-7], self.epoch))
+                    pbar.update(1)
+                    self.eval_writer.add_scalar('Metric/Loss', loss.item(), self.epoch * len(data_loader) + batch_idx)
+                    self.eval_writer.add_scalar('Metric/Model Entropy', Categorical(logits=output).entropy().mean().item(), self.epoch * len(data_loader) + batch_idx)
+                    self.eval_writer.add_scalar('Metric/Accuracy', accuracy, self.epoch * len(data_loader) + batch_idx)
+                    self.eval_writer.add_scalar('Metric/Recall', recall, self.epoch * len(data_loader) + batch_idx)
+                    self.eval_writer.add_scalar('Metric/Precision', precision, self.epoch * len(data_loader) + batch_idx)
+                    self.eval_writer.add_scalar('Metric/F1 Score', f1_score, self.epoch * len(data_loader) + batch_idx)
 
     def save(self):
         epoch_path = os.path.join(self.experiment_path, 'epoch_%06d' % self.epoch)
--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 from pipetorch.experiment import Experiment
 import torch
 from torch import nn
+from torch.utils import data
 import torchvision
 import torch.optim as optim
 import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--batch_size_train', type=int, default=5000, required=False, help="Batch size during training.")
-parser.add_argument('--batch_size_test', type=int, default=1, required=False, help="Batch size during testing.")
+parser.add_argument('--batch_size', type=int, default=5000, required=False, help="Batch size.")
 parser.add_argument('--learning_rate', type=float, default=1e-3, required=False, help="Learning rate.")
-parser.add_argument('--weight_decay', type=float, default=0.2, required=False, help="Weight decay.")
+parser.add_argument('--weight_decay', type=float, default=0.0, required=False, help="Weight decay.")
 parser.add_argument('--experiments_path', type=str, default=r'C:\Users\omrijsharon\Documents\Experiments', required=False, help="Full path of the experiments directory for TensorBoard and Checkpoints.")
 
 
@@ -21,35 +21,42 @@ def main():
     # model = torchvision.models.mobilenet_v3_large(False)
     model = nn.Sequential(torchvision.models.mobilenet_v3_large(False), nn.Linear(1000, 10))
     # model = nn.Sequential(torchvision.models.mobilenet_v3_small(False), nn.Linear(1000, 10))
-    batch_size_train = args.batch_size_train
-    batch_size_test = args.batch_size_test
+    batch_size = args.batch_size
     learning_rate = args.learning_rate
     weight_decay = args.weight_decay
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 5)
-    train_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST('/files/', train=True, download=True,
-                                   transform=torchvision.transforms.Compose([
-                                       torchvision.transforms.ToTensor(),
-                                       torchvision.transforms.Normalize(
-                                           (0.1307,), (0.3081,))
-                                   ])),
-        batch_size=batch_size_train, shuffle=True)
+    # Weight decay scheduler? increase weight_decay when loss goes down
+    # Dropout scheduler? increase dropout rate when loss goes down
+    entire_train_dataset = torchvision.datasets.MNIST('/files/', train=True, download=True,
+                                                      transform=torchvision.transforms.Compose([
+                                                          torchvision.transforms.ToTensor(),
+                                                          torchvision.transforms.Normalize(
+                                                              (0.1307,), (0.3081,))
+                                                          ]))
 
-    test_loader = torch.utils.data.DataLoader(
-        torchvision.datasets.MNIST('/files/', train=False, download=True,
-                                   transform=torchvision.transforms.Compose([
-                                       torchvision.transforms.ToTensor(),
-                                       torchvision.transforms.Normalize(
-                                           (0.1307,), (0.3081,))
-                                   ])),
-        batch_size=batch_size_test, shuffle=True)
+    split_dataset = data.random_split(
+        entire_train_dataset,[int(0.8 * len(entire_train_dataset)), int(0.2 * len(entire_train_dataset))]
+    )
+    train_set = split_dataset[0]
+    validation_set = split_dataset[1]
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+    validation_loader = torch.utils.data.DataLoader(validation_set, batch_size=batch_size, shuffle=True)
+
+    test_dataset = torchvision.datasets.MNIST('/files/', train=False, download=True,
+                                              transform=torchvision.transforms.Compose([
+                                                  torchvision.transforms.ToTensor(),
+                                                  torchvision.transforms.Normalize(
+                                                      (0.1307,), (0.3081,))
+                                                  ]))
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
     def preprocess(data):
         return data.expand(data.size(0), 3, data.size(2), data.size(3))
 
     path = args.experiments_path
-    experiment = Experiment(model, train_loader, torch.nn.CrossEntropyLoss(), optimizer, path, scheduler=scheduler,
+    experiment = Experiment(model, train_loader, validation_loader, torch.nn.CrossEntropyLoss(), optimizer, path, scheduler=scheduler,
                             data_preprocess_function=preprocess)
     experiment.run(delta_epochs_to_save_checkpoint=10)
 
